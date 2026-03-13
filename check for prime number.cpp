593dcb9e91f8f960d60934e57741d15885d00966
nidhis071 <nidhis275411@gmail.com>
2026-03-13 11:57:39 +0530

    Create check for prime number.cpp

---
+++ check for prime number.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num, i, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for(i = 1; i <= num; i++)
+    {
+        if(num % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if(count == 2)
+        cout << "Prime number";
+    else
+        cout << "Not a prime number";
+
+    return 0;
+}
